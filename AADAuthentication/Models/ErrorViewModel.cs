acb29ef912a5d17088d9e05cbc0b43d5ec058d7f
pradeeponsuccess <Pradeepvaishya84@gmail.com>
2023-05-14 00:03:53 +0530

    Add project files.

---
+++ AADAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AADAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AADAuthentication/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+        .AddAzureAD(options => builder.Configuration.Bind("AzureAd", options));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
